9d53f880a2cfcd48b6c708afa35512450aeb99e6
Susanta Ray <susantaray786@gmail.com>
2025-01-14 01:15:53 +0530

    frontend: Creating chat area and task area UI ✔

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,17 +3,19 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Login from "../screens/Login";
 import Home from "../screens/Home";
 import Register from "../screens/Register";
+import Project from "../screens/Project";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
 export default AppRoutes;
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,11 +1,15 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../context/user.context";
 import axios from "../config/axios";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const { user } = useContext(UserContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [projectName, setProjectName] = useState(null);
+  const [project, setProject] = useState([]);
+
+  const navigate = useNavigate();
 
   const createProject = (e) => {
     e.preventDefault();
@@ -24,9 +28,20 @@ const Home = () => {
       });
   };
 
+  useEffect(() => {
+    axios
+      .get("/projects/all")
+      .then((res) => {
+        setProject(res.data.projects);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <main className="p-4">
-      <div className="projects">
+      <div className="projects flex flex-wrap gap-3">
         <button
           onClick={() => setIsModalOpen(true)}
           className="project p-4 border border-slate-300 rounded-md"
@@ -34,6 +49,32 @@ const Home = () => {
           New Project
           <i className="ri-link ml-2"></i>
         </button>
+
+        {project.map((p) => (
+          <div
+            onClick={() => {
+              navigate("/project", {
+                state: { project },
+              });
+            }}
+            key={p._id}
+            className="project flex flex-col gap-2 cursor-pointer p-4 border 
+            border-slate-300 rounded-md min-w-52 hover:bg-slate-200"
+          >
+            <h2 className="font-semibold">{p.name}</h2>
+
+            <div className="flex gap-2">
+              <p>
+                {" "}
+                <small>
+                  <i className="ri-user-line"></i>
+                </small>{" "}
+                <small>Collaborators</small> :{" "}
+              </p>
+              {p.users.length}
+            </div>
+          </div>
+        ))}
       </div>
 
       {isModalOpen && (
