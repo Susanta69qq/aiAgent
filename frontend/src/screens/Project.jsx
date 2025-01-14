ff469e90fd348b2493ef4453287d0447fc8f896a
Susanta Ray <susantaray786@gmail.com>
2025-01-14 16:06:56 +0530

    frontend: UI integration where all the users associated with the project can be seen with their emailId's in a panel ✔

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -3,13 +3,13 @@ import { useLocation } from "react-router-dom";
 import axios from "../config/axios";
 
 const Project = () => {
+  const location = useLocation();
+
   const [isSidePanelOpen, setIsSidePanelOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedUserId, setSelectedUserId] = useState([]);
   const [users, setUsers] = useState([]);
-
-  const location = useLocation();
-  console.log(location.state);
+  const [project, setProject] = useState(location.state.project);
 
   const handleUserClick = (id) => {
     setSelectedUserId((prevSelectedUserId) => {
@@ -40,6 +40,13 @@ const Project = () => {
   };
 
   useEffect(() => {
+    axios
+      .get(`/projects/get-project/${location.state.project._id}`)
+      .then((res) => {
+        console.log(res.data.project);
+        setProject(res.data.project);
+      });
+
     axios
       .get("/users/all")
       .then((res) => {
@@ -94,23 +101,33 @@ const Project = () => {
           className={`sidePanel w-full h-full flex flex-col gap-2 bg-slate-50 absolute transition-all duration-300 
             ${isSidePanelOpen ? "translate-x-0" : "-translate-x-full"} top-0`}
         >
-          <header className="flex justify-end px-4 p-2 w-full bg-slate-200">
+          <header className="flex justify-between items-center px-4 p-2 w-full bg-slate-200">
+            <h1 className="font-semibold text-lg">Collaborators</h1>
+
             <button onClick={() => setIsSidePanelOpen(!isSidePanelOpen)}>
               <i className="ri-close-fill"></i>
             </button>
           </header>
 
           <div className="users flex flex-col gap-2">
-            <div className="user cursor-pointer hover:bg-slate-200 p-2 flex gap-2 items-center">
-              <div
-                className="aspect-square rounded-full w-fit h-fit flex items-center justify-center p-5 
+            {project.users &&
+              project.users.map((user) => {
+                return (
+                  <div
+                    key={user}
+                    className="user cursor-pointer hover:bg-slate-200 p-2 flex gap-2 items-center"
+                  >
+                    <div
+                      className="aspect-square rounded-full w-fit h-fit flex items-center justify-center p-5 
                     text-white bg-slate-600"
-              >
-                <i className="ri-user-fill absolute"></i>
-              </div>
+                    >
+                      <i className="ri-user-fill absolute"></i>
+                    </div>
 
-              <h1 className="font-semibold text-lg">username</h1>
-            </div>
+                    <h1 className="font-semibold text-lg">{user.email}</h1>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </section>
