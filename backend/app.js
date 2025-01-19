b4b28761c796d7760e452b26e003e21949821b98
Susanta Ray <susantaray786@gmail.com>
2025-01-20 02:43:17 +0530

    backend: app.js ......

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://aicaproject.netlify.app/"
+  origin: "https://aicaproject.netlify.app"
 }));
 
 app.get("/health", (req, res) => {
