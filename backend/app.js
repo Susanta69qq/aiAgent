217cf53e12a19ae840099b8637bcfd2cd84064bc
Susanta Ray <susantaray786@gmail.com>
2025-01-20 02:22:17 +0530

    backend: cors origin updated ✔

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,9 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: "https://aicaproject.netlify.app/"
+}));
 
 app.get("/health", (req, res) => {
   res.send("Ok!");
